966cb8240fb117fc737fd6429adf61fd67c9af89
Tinnuadan <tinnuadan@gmail.com>
2020-01-14 16:39:38 +0100

    logging in config loading

--- src/main/java/de/docm77/patreon/nicesleep/Config.java
+++ src/main/java/de/docm77/patreon/nicesleep/Config.java
@@ -1,5 +1,6 @@
 package de.docm77.patreon.nicesleep;
 
+import java.lang.Class;
 import java.util.HashMap;
 
 import org.bukkit.boss.BarColor;
@@ -10,35 +11,58 @@ final class Config {
   public enum Bar {
     Player, OP
   }
+
+  private JavaPlugin plugin;
   public int neededPercentage;
   public double skipDelaySeconds;
   public boolean opsCanOverride;
   public HashMap<Bar, BarColor> barColors;
 
-  public void log(JavaPlugin plugin) {
+  Config(JavaPlugin plugin) {
+    this.plugin = plugin;
+  }
+
+  public void log() {
     plugin.getLogger().info("Settings:");
     plugin.getLogger().info("\tpercentage_needed: " + neededPercentage);
     plugin.getLogger().info("\tseconds_before_skip: " + skipDelaySeconds);
     plugin.getLogger().info("\tops_can_override: " + opsCanOverride);
   }
 
-  public void load(JavaPlugin plugin) {
+  public void load() {
     barColors = new HashMap<Bar, BarColor>();
     FileConfiguration config = plugin.getConfig();
-    neededPercentage = Math.min(100, Math.max(0, config.getInt("percentage_needed")));
-    skipDelaySeconds = Math.max(0.0, config.getDouble("seconds_before_skip"));
-    opsCanOverride = config.getBoolean("ops_can_override");
-    barColors.put(Bar.Player, strToColor(config.getString("barcolor.player"), plugin));
-    barColors.put(Bar.OP, strToColor(config.getString("barcolor.op"), plugin));
+
+    neededPercentage = loadValue(config, "percentage_needed", int.class, 0);
+    neededPercentage = Math.min(100, Math.max(0, neededPercentage));
+
+    skipDelaySeconds = loadValue(config, "seconds_before_skip", double.class, 4.0);
+    skipDelaySeconds = Math.min(15., Math.max(0., skipDelaySeconds));
+
+    opsCanOverride = loadValue(config, "ops_can_override", boolean.class, false);
+
+    String tmp;
+    tmp = loadValue(config, "barcolor.player", String.class, "WHITE");
+    barColors.put(Bar.Player, strToColor(tmp));
+    tmp = loadValue(config, "barcolor.op", String.class, "WHITE");
+    barColors.put(Bar.OP, strToColor(tmp));
   }
 
-  private BarColor strToColor(String color, JavaPlugin plugin) {
+  private BarColor strToColor(String color) {
     BarColor bc = BarColor.WHITE;
     try {
       bc = BarColor.valueOf(color.toUpperCase());
-    } catch(IllegalArgumentException e) {
+    } catch (IllegalArgumentException e) {
       plugin.getLogger().warning("Unable to convert " + color + " into a bar color. Falling back to white");
     }
     return bc;
+  }
+
+  private <T> T loadValue(FileConfiguration config, String path, Class<T> cl, T def) {
+    if (config.get(path, null) == null) {
+      plugin.getLogger().info("Config for " + path + " not found, using default value (" + def + ")");
+      return def;
+    }
+    return config.getObject(path, cl);
   }
 }
