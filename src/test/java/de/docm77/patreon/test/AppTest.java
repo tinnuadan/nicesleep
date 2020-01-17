e550854a4a0ff1ebbf89ef03e0e635df3df2c6aa
Tinnuadan <tinnuadan@gmail.com>
2020-01-17 10:42:33 +0100

    some tests

--- src/test/java/de/docm77/patreon/test/AppTest.java
+++ src/test/java/de/docm77/patreon/test/AppTest.java
@@ -8,12 +8,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.math.RoundingMode;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 import de.docm77.patreon.nicesleep.Config;
+import de.docm77.patreon.nicesleep.Utils;
 import de.docm77.patreon.nicesleep.Config.Bar;
 
 /**
@@ -25,7 +27,21 @@ public class AppTest {
    */
   @Test
   public void testApp() {
+    testUtils();
     testConfig();
+
+  }
+
+  public void testUtils() {
+    assertEquals(11, Utils.Round(10.5, RoundingMode.CEILING));
+    assertEquals(11, Utils.Round(10.5, RoundingMode.UP));
+    assertEquals(10, Utils.Round(10.5, RoundingMode.DOWN));
+    assertEquals(10, Utils.Round(10.5, RoundingMode.FLOOR));
+    assertEquals(10, Utils.Round(10.5, RoundingMode.HALF_DOWN));
+    assertEquals(11, Utils.Round(10.5, RoundingMode.HALF_UP));
+    assertEquals(10, Utils.Round(10.5, RoundingMode.HALF_EVEN));
+    assertEquals(12, Utils.Round(11.5, RoundingMode.HALF_EVEN));
+    assertEquals(12, Utils.Round(12.0, RoundingMode.UNNECESSARY));
   }
 
   public void testConfig() {
@@ -45,6 +61,7 @@ public class AppTest {
     assertEquals(55, config.neededPercentage);
     assertEquals(4.1, config.skipDelaySeconds, 0.1);
     assertEquals(true, config.opsCanOverride);
+    assertEquals(RoundingMode.CEILING, config.roundingMethod);
     assertEquals(BarColor.BLUE, config.barColors.get(Bar.Player));
     assertEquals(BarColor.PINK, config.barColors.get(Bar.OP));
   }
